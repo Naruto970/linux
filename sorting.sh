273dfad9d83a78c050567534689e874a29252839
mugiwarapirates <sargampandey27oct@gmail.com>
2019-11-11 19:11:20 +0530

    pushed from Test4

--- sorting.sh
+++ sorting.sh
@@ -1,4 +1,11 @@
+#!/bin/bash -x
+declare -A  Test
+
 read  -p "enter the no . of a: " a
 read -p "enter the value of b: " b
 read -p "enter the value of c: " c
 echo $a $b $c
+
+Test[Case4]=$(( ($a % $b) +$c ))  
+echo ${Test[Case4]}
+
