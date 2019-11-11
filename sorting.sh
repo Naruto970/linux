46e6cefefccac0a78ce96fdaee027fc6d60659e1
mugiwarapirates <sargampandey27oct@gmail.com>
2019-11-11 18:28:09 +0530

    committed from Test2

--- sorting.sh
+++ sorting.sh
@@ -1,4 +1,13 @@
+#!/bin/bash -x
+declare -A Test
+
 read  -p "enter the no . of a: " a
 read -p "enter the value of b: " b
 read -p "enter the value of c: " c
 echo $a $b $c
+
+Test[Case2]=$(( ($a * $b) + $c ))
+echo ${Test[Case2]} 
+
+
+
