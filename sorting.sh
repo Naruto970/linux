32102848f1cc117eb7352e9e53b719687b9264bf
mugiwarapirates <sargampandey27oct@gmail.com>
2019-11-11 18:42:17 +0530

    updated from Test3

--- sorting.sh
+++ sorting.sh
@@ -1,4 +1,11 @@
+#!/bin/bash -x
+declare -A Test
 read  -p "enter the no . of a: " a
 read -p "enter the value of b: " b
 read -p "enter the value of c: " c
 echo $a $b $c
+
+
+Test[Case3]=$(( ($c + $a) / $b ))
+echo ${Test[Case3]}
+
