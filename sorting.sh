7ab7a826d564fca5ee4adae028a3554e28578c1a
mugiwarapirates <sargampandey27oct@gmail.com>
2019-11-12 10:27:26 +0530

    stored in the dictionary

--- sorting.sh
+++ sorting.sh
@@ -1,17 +1,32 @@
 #!/bin/bash -x
 declare -A  Test
 
+valid=true
+counter=0
+
 read  -p "enter the no . of a: " a
 read -p "enter the value of b: " b
 read -p "enter the value of c: " c
 echo $a $b $c
 
-Test[Case1]=$(( ($a + $b) * $c ))
+Test[1]=$(( ($a + $b) * $c ))
  echo ${Test[Case1]}
-Test[Case2]=$(( ($a * $b) + $c ))
+Test[2]=$(( ($a * $b) + $c ))
 echo ${Test[Case2]} 
-Test[Case3]=$(( ($c + $a) / $b ))
+Test[3]=$(( ($c + $a) / $b ))
 echo ${Test[Case3]}
-Test[Case4]=$(( ($a % $b) +$c ))  
+Test[4]=$(( ($a % $b) +$c ))  
 echo ${Test[Case4]}
 
+ while [ $valid ]
+do
+   if [ $counter -le 4 ]
+  then
+   Input[((counter++))]=${Test[$counter]}
+   else
+    {
+	break
+    }
+  fi
+   done
+echo ${Input[@]}
